9b0300680da91e6688d4f2bbd3033fadf31bc586
LuisL <dev.eng.luisluna@gmail.com>
2023-02-16 21:12:54 +0000

    version con el carrousel tipo Netflix ----> No terminado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const carouselItems = document.querySelector(".carousel-items");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+let itemIndex = 0;
+const itemCount = carouselItems.children.length;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+prevButton.addEventListener("click", () => {
+  if (itemIndex > 0) {
+    itemIndex--;
+    carouselItems.style.transform = `translateX(-${itemIndex * 25}%)`;
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (itemIndex < itemCount - 1) {
+    itemIndex++;
+    carouselItems.style.transform = `translateX(-${itemIndex * 25}%)`;
+  }
+});
+
+// Modulo para hacer el carrusel automatico
+
+// setInterval(() => {
+//   if (itemIndex < itemCount - 1) {
+//     itemIndex++;
+//     carouselItems.style.transform = `translateX(-${itemIndex * 25}%)`;
+//   } else {
+//     itemIndex = 0;
+//     carouselItems.style.transform = "translateX(0)";
+//   }
+// }, 5000);
